04c522cb94203697070ac9c237ee8ddc2e6a916f
Serah-Njenga <serah35.nn@gmail.com>
2023-03-29 13:56:59 +0300

    Done

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(str) {
+  // convert the string to uppercase
+  str = str.toUpperCase();
+
+  let start = 0;
+  let end = str.length -1;
+   while(start <= end) {
+    if(str[start] != str[end]){
+      return false ;
+    }
+    start++;
+    end--;
+  }
+
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  decremengt end 
+  increment start
+  if elements do not match return false 
+  check if last element != element at beginning
+  loop through the string
 */
 
 /*
   Add written explanation of your solution here
+  A palindrome is a word or phrase that reads the same backward and forward. When palindromes are the length of a sentence, they ignore capitalization, punctuation, and word boundaries.
 */
 
 // You can run `node index.js` to view these console logs
